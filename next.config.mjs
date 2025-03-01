fb586da68b7da5231b3007b5b6a0ce3306d04faf
Neil Mallia <neil.mallia@9hdigital.com>
2025-03-01 17:04:23 +0100

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	experimental: {
-		appDir: true,  // Ensures App Router is enabled
-	},
 };
 
 export default nextConfig;
