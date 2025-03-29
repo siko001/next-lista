39e5536509d5feaa82d5279a1df95a0aa6c1d5db
Neil Mallia <neil.mallia@9hdigital.com>
2025-03-30 00:38:30 +0100

    logout functional, fix register bug

--- app/logout/page.js
+++ app/logout/page.js
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers'
 
 export default async function LogoutPage() {
     const cookieStore = await cookies()
+    const token = cookieStore.get('hodowipefhwfg8wgfd687gbbru3fg3bfgh3297fgh2e7g3hg')?.value
     const reg = cookieStore.get('hodowipefhwfg8wgfd687gbbru3fg3bfgh3297fgh2e7g3hghodowipefhwfg8wgfd687gbbru3fg3bfgh3297fgh2e7g3hg')?.value
 
     if (!token || reg === "no") {
