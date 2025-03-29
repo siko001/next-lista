37e40ebfa1a6c5f52238d1aedd2b534f865427dc
Neil Mallia <neil.mallia@9hdigital.com>
2025-03-30 00:31:05 +0100

    logout functional, fix register bug

--- app/logout/page.js
+++ app/logout/page.js
@@ -1,7 +1,7 @@
 'use server'
 import { redirect } from 'next/navigation'
 import LogoutClient from './LogoutClient'
-import { getAuthCookies } from '../lib/AuthCookies'
+import { getAuthCookies } from '@/lib/AuthCookies'
 
 export default async function LogoutPage() {
     const { token, reg } = await getAuthCookies();
