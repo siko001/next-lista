2825c933aa4130bb45589c806ca214b6c8b844a0
Neil Mallia <neil.mallia@9hdigital.com>
2025-05-25 18:43:27 +0200

    fix bug

--- app/components/parts/List.js
+++ app/components/parts/List.js
@@ -42,7 +42,7 @@ export default function List({
                 setListMetadata(metadata);
             });
         }
-    }, [list, userData?.id]);
+    }, [list]);
 
     const handleGoToList = (e) => {
         e.stopPropagation();
