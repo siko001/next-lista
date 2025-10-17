06158f02ed8d74c252eab6e58230ff9a5b648317
Neil Mallia <neil.mallia@9hdigital.com>
2025-10-17 16:24:31 +0200

    Added serve validation

--- app/password-reset/page.js
+++ app/password-reset/page.js
@@ -1,5 +1,14 @@
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
 import PasswordResetClient from "./PasswordResetClient";
-
 export default function Page() {
+    const cookieStore = cookies();
+    const cookieToken = cookieStore.get("token")?.value;
+    const registered = cookieStore.get("registered")?.value;
+
+    // If already registered/logged in, send home
+    if (cookieToken && registered === "yes") {
+        redirect("/");
+    }
     return <PasswordResetClient />;
 }
--- app/reset-password/page.js
+++ app/reset-password/page.js
@@ -1,11 +1,25 @@
-"use client";
-import {useSearchParams} from "next/navigation";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
 import ResetPasswordClient from "./ResetPasswordClient";
 
-export default function ResetPasswordPage() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const key = searchParams.get("key");
-  const login = searchParams.get("login");
-  return <ResetPasswordClient token={token} resetKey={key} login={login} />;
+export default function ResetPasswordPage({searchParams}) {
+    const cookieStore = cookies();
+    const cookieToken = cookieStore.get("token")?.value;
+    const registered = cookieStore.get("registered")?.value;
+
+    const token = searchParams?.token || null;
+    const key = searchParams?.key || null;
+    const login = searchParams?.login || null;
+
+    // If no valid params are present, redirect to login
+    if (!token && !(key && login)) {
+        redirect("/login");
+    }
+
+    // If already registered/logged in and token flow present, send home
+    if (cookieToken && registered === "yes") {
+        redirect("/");
+    }
+
+    return <ResetPasswordClient token={token} resetKey={key} login={login} />;
 }
